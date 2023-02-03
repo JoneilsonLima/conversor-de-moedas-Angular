0c55c48f379903c157422dcccacb519641165eba
joneilson <joneilson84@gmail.com>
2023-02-03 17:47:49 -0300

    Teste: teste de serviço adicionado

--- src/app/services/moedas.service.spec.ts
+++ src/app/services/moedas.service.spec.ts
@@ -1,16 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { MoedasService } from './moedas.service';
 
-describe('MoedasService', () => {
+fdescribe('MoedasService', () => {
   let service: MoedasService;
+  let http: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+          HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(MoedasService);
+    http = TestBed.inject(HttpClient)
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('Must call a GET with the correct endpoint', () => {
+    const spy = spyOn(http, 'get').and.callThrough()
+    service.getSymbols()
+    expect(spy).toHaveBeenCalledWith('https://api.exchangerate.host/symbols')
+  })
 });
+
